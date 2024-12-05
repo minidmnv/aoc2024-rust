01a394832c68cd55f3650889c6724cc80313aa41
Michał Niciński <minidmnv@gmail.com>
2024-12-05 07:24:50 +0100

    day04 solution

--- src/day04.rs
+++ src/day04.rs
@@ -1,15 +1,85 @@
+use std::os::unix::raw::uid_t;
+
 pub fn run(input: &str) {
-    part_one(&input);
-    // part_two(&input);
+
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    part_one(grid.clone());
+    part_two(grid.clone());
 }
 
-fn part_one(input: &&str) {
+fn part_one(grid: Vec<Vec<char>>) {
+    const XMAS: &str = "XMAS";
 
-    println!("Day 4, part 1 result: {}", input);
+    let result: i32 = grid.iter().enumerate().map(|(x, row)| row.iter().enumerate().map(|(y, _value)| match_word_line_in_grid(grid.clone(), x, y, XMAS)).sum::<i32>()).sum::<i32>();
+
+    println!("Day 4, part 1 result: {}", result);
 }
 
-fn part_two(input: &&str) {
+fn match_word_line_in_grid(grid: Vec<Vec<char>>, x: usize, y: usize, word: &str) -> i32 {
+    let height = grid.len();
+    let width = grid[0].len();
+    let mut count: i32 = 0;
 
-    println!("Day 4, part 2 result: {}", input);
+    if grid[x][y] != word.chars().next().unwrap() {
+        return 0
+    }
+
+    let word_length = word.len();
+
+    let x_dirs: [i32; 8] = [-1, -1, -1, 0, 0, 1, 1, 1];
+    let y_dirs: [i32; 8] = [-1, 0, 1, 1, -1, -1, 0, 1];
+
+    for dir_i in 0..x_dirs.len() {
+
+        let mut curr_x: i32 = x as i32;
+        let mut curr_y: i32 = y as i32;
+
+        for k in 1..word_length {
+            curr_x += x_dirs[dir_i];
+            curr_y += y_dirs[dir_i];
+
+            if curr_x < 0 || curr_x >= height as i32 || curr_y < 0 || curr_y >= width as i32 { break }
+            if word.chars().nth(k).unwrap() != grid[curr_x as usize][curr_y as usize] { break }
+
+            if k == word_length - 1 { count += 1 }
+        }
+    }
+    count
+}
+
+fn match_x_mas_in_grid(grid: Vec<Vec<char>>, x: usize, y: usize) -> i32 {
+    const CENTER_CHAR: char = 'A';
+
+    let height = grid.len();
+    let width = grid[0].len();
+
+    if grid[x][y] != CENTER_CHAR {
+        return 0
+    }
+
+    let x_dirs: [i32; 4] = [-1, 1, -1, 1];
+    let y_dirs: [i32; 4] = [-1, 1, 1, -1];
+
+    let result_array: Vec<char> = (0..x_dirs.len()).map(|dir_i| {
+        let curr_x: i32 = x as i32 + x_dirs[dir_i];
+        let curr_y: i32 = y as i32 + y_dirs[dir_i];
+
+        if curr_x < 0 || curr_x >= height as i32 || curr_y < 0 || curr_y >= width as i32 { return 'X' }
+
+        grid[curr_x as usize][curr_y as usize]
+    }).collect();
+
+    if result_array[0] == result_array[1] || result_array[2] == result_array[3] || result_array.iter().filter(|&&cr| cr == 'M').count() != 2 || result_array.iter().filter(|&&cr| cr == 'S').count() != 2 { 0 } else { 1 }
+}
+
+fn part_two(grid: Vec<Vec<char>>) {
+
+    let result: i32 = grid.iter().enumerate().map(|(x, row)| row.iter().enumerate().map(|(y, _value)| match_x_mas_in_grid(grid.clone(), x, y)).sum::<i32>()).sum::<i32>();
+
+    println!("Day 4, part 2 result: {}", result);
 }
 
